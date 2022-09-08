6c3b3f5152d92f93bc4d5180dff2fcb67deeb15f
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-08 13:23:41 -0700

    oya

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - the entry point to the program
- * this program prings a set of strings and 
+ * main - Entry point
+ * this program prings a set of strings and
  * returns 0
  */
 int main(void)
