4163c77826a2d6f28ccd8b3e731786e97b2ff618
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-08 13:52:36 -0700

    oh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main: Entry point
- * Description: prints using the main
+ * main: print the sting in the printf function
+ *
+ * Description: using the main function
  * Return: 0
  */
 int main(void)
