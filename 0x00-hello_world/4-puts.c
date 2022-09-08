7ea209df9610dfd5d10ed39d8b8282fbe65f2d23
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-08 13:31:48 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: prints "programming is like building a multilingual puzzle"
- * return: 0
+ * Description: prints "\"programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
