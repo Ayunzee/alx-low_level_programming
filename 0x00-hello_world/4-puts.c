2e04d208337d393722d118c3238b07e3ffc4dbbe
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-08 13:11:41 -0700

    wow

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#incluse <stdio.h>
+/**
+ * main - the entry point to the program
+ * this program prings a set of strings
+ * returns 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
