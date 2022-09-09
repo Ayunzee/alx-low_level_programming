97200c95c3375d7fa895979b12df6da149f4434b
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-09 15:14:33 -0700

    ghgt

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+char n;
+for (n = '0' ; n <= '9' ; n++)
+{
+	putchar(n);
+}
+for (n = '9' ; n >= '0' ; n--)
+{
+	putchar(n);
+}
+putchar(',');
+putchar('\n');
+return (0);
+}
