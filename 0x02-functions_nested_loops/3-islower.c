6534eb7f51e40caa335befe193386c1a33059652
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-13 10:33:21 -0700

    hgh

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print abc and new line
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
