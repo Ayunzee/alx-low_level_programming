0f002106f232e1b040feb736fca77352f2aff6c7
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-13 16:20:13 -0700

    hty

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds the integers
+ * @num1: holds variable for the first number
+ * @num2: holds second variable for the second number
+ *
+ * Return: the summation
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
