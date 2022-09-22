fda892d68ad692f7e983995f7e0d68c0a76eccba
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-22 13:59:16 -0700

    hey

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,19 +3,19 @@
 /**
  * string_toupper - changes all lowercase letters to uppercase
  * @str: source string to be changed
+ *
  * Return: address tp the string
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int i = 0;
-	char str;
 
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
-			i++;
+		i++;
 	}
 	return (str);
 }
