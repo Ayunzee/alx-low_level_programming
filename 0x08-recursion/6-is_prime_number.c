f9ea5c24350866950b0fb37a90fe155eea364ad4
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-28 16:29:14 -0700

    hello

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,3 +15,20 @@ int is_prime_number(int n)
 		return (0);
 	return (is_prime(n, start));
 }
+
+/**
+ * is_prime - returns prime numbers
+ *@n: number to be checked
+ *@start: where to start
+ *
+ * Return: 1or 0
+ */
+
+int is_prime(int n, int start)
+{
+	if (start <= 1)
+		return (1);
+	else if (n % start == 0)
+		return (0);
+	return (is_prime(n, start - 1));
+}
