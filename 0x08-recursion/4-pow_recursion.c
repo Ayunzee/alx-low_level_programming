e86ec1e5a67e18d0808d80eb4192905443ff6fd3
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-28 14:52:07 -0700

    hello

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raises anint to the power of anther
+ * @x: the int
+ * @y: the power
+ *
+ * Return: the value
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (+1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
