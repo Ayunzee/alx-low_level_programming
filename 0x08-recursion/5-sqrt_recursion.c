65d726c41d6f609ead3e034e62447f9d6b259916
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-28 16:15:55 -0700

    hello

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,3 +13,20 @@ int _sqrt_recursion(int n)
 		return (n);
 	return (_sqrt(0, n));
 }
+/**
+ * _sqrt - returns the square root
+ *@n: digit to be raised
+ *@x: n raused to tghe power of x
+ *
+ *Return: the square root of n
+ */
+
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
+}
+
