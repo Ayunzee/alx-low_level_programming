c81724bb1fa7f857abb40ebbb0ffd7f5ffa07a63
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-28 15:03:27 -0700

    hello

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the square root of a number
+ *@n: number whose sqrt is given
+ *
+ * Return: sqrt
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 1 || n == 0)
+		return (n);
+	return (_sqrt(0, n));
+}
