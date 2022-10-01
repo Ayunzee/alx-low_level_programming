b2cf0e770bf02cffe499e8a1f8aa9db7ebc11b08
Ayoade <oluwafemiayoade13@gmail.com>
2022-09-30 17:18:22 -0700

    hry

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - orints its name, followed by a new line
  * @argc: argument that counts argument input
- * @argv: argument that stores the strings in an array or char* 
- *
+ * @argv: argument that stores the strings in an array or char* (strings)
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
 	int num_1, num_2, mul;
