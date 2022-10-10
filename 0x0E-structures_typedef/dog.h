afcdf92b6a55d78eb962e3717f1a981b0c500ccf
Ayoade <oluwafemiayoade13@gmail.com>
2022-10-10 13:08:41 -0700

    hello

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * struct dog - the struct defined
+ * @name: name entered
+ * @age: age of thr struct
+ * @owner: owner of the struct
+ *
+ * Description: defines dog as a structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	return (0);
+}
